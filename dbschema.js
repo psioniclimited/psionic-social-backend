e080c26f7abc8db7a123086d90bac6248db1bcce
sabbir-anwar <protik26@gmail.com>
2020-10-15 14:40:54 +0600

    userdetail incomplete

--- dbschema.js
+++ dbschema.js
@@ -1,4 +1,15 @@
 let db = {
+  users: [{
+    userId: 'jhgjh44hcajh6876avhav',
+    email: 'user@gmail.com',
+    handle: 'user',
+    createdAt: '2020-10-13T11:54:57.600Z',
+    imageUrl: 'image/sdadafasfaf/afafagasg',
+    bio: 'hi there, nice to meet you',
+    website: 'https://user.com',
+    location: 'dhaka, bangladesh'
+  }
+  ],
   scream: [
     {
       userHandle: 'user',
